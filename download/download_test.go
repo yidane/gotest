affd93e853bb95118a1321521592ff406cc831e4
yidane <yidane@outlook.com.cn>
2017-05-24 22:54:01 +0800

    home change

--- download/download_test.go
+++ download/download_test.go
@@ -17,11 +17,11 @@ import (
 // 	}
 // }
 
-// func Benchmark_Download(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
-// 		Download()
-// 	}
-// }
+func Benchmark_Download(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Download()
+	}
+}
 
 // func Test_Visit(t *testing.T) {
 // 	for i := 0; i < 10000; i++ {
@@ -36,27 +36,27 @@ import (
 // 	}
 // }
 
-func Test_Chanel(t *testing.T) {
-	fmt.Println("start")
-	chn := make(chan Result)
-	cpuSum := runtime.NumCPU()
-	runtime.GOMAXPROCS(cpuSum)
-	for i := 0; i < cpuSum; i++ {
-		//go ChanelDownlowd("http://www.kcaogen.top/blogInfo?blogId=11", i, chn)
-		//go ChanelDownlowd("http://localhost/TestWeb/api/tick", i, chn)
-		//go ChanelDownlowd("https://www.baidu.com/", i, chn)
+// func Test_Chanel(t *testing.T) {
+// 	fmt.Println("start")
+// 	chn := make(chan Result)
+// 	cpuSum := runtime.NumCPU()
+// 	runtime.GOMAXPROCS(cpuSum)
+// 	for i := 0; i < cpuSum; i++ {
+// 		//go ChanelDownlowd("http://www.kcaogen.top/blogInfo?blogId=11", i, chn)
+// 		//go ChanelDownlowd("http://localhost/TestWeb/api/tick", i, chn)
+// 		//go ChanelDownlowd("https://www.baidu.com/", i, chn)
 
-		go ChanelDownlowd("http://baike.baidu.com/api/wikiui/sharecounter?lemmaId=20350316&method=add&type=like", i, chn)
-	}
+// 		//go ChanelDownlowd("http://baike.baidu.com/api/wikiui/sharecounter?lemmaId=20350316&method=add&type=like", i, chn)
+// 		go ChanelDownlowd("http://127.0.0.1:8022/", i, chn)
+// 	}
 
-	total := 0
-	for i := 0; i < cpuSum*10000; i++ {
-		<-chn
-		total++
-		fmt.Println(total)
-		//fmt.Println(<-chn
-	}
-}
+// 	total := 0
+// 	for i := 0; i < cpuSum*10000; i++ {
+// 		<-chn
+// 		total++
+// 		fmt.Println(total)
+// 	}
+// }
 
 func ChanelDownlowd(url string, id int, chn chan Result) {
 	for i := 0; i < 10000; i++ {

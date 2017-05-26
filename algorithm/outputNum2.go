d75f483cd730c93e77004368c81235220411edb6
yidane <yidane@163.com>
2017-05-26 18:12:51 +0800

    输出1,2,3,4

--- algorithm/outputNum2.go
+++ algorithm/outputNum2.go
@@ -1,23 +1,28 @@
 package main
 
 import "time"
+import "fmt"
+
+var arr []int = make([]int, 0)
 
 func output2() {
-	cha := make(chan struct{})
-	chb := make(chan struct{})
-	chc := make(chan struct{})
-	chd := make(chan struct{})
+	cha := make(chan []int)
+	chb := make(chan []int)
+	chc := make(chan []int)
+	chd := make(chan []int)
 	go test(1, cha, chb)
 	go test(2, chb, chc)
 	go test(3, chc, chd)
 	go test(4, chd, cha)
-	cha <- struct{}{}
-	time.Sleep(time.Second * 2) //阻塞
+	cha <- []int{}
+	time.Sleep(time.Second / 100) //阻塞
+
+	fmt.Println(arr)
 }
 
-func test(i int, ch1, ch2 chan struct{}) {
+func test(i int, ch1, ch2 chan []int) {
 	for range ch1 {
-		println(i)
-		ch2 <- struct{}{}
+		arr = append(arr, i)
+		ch2 <- []int{}
 	}
 }

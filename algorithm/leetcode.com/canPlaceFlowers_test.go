5ea02696bafee966e0f950fbabe4b5384c8fe5bc
yidane <yidane@outlook.com.cn>
2017-06-07 00:42:56 +0800

    添加测试用例，测试花坛放置算法

--- algorithm/leetcode.com/canPlaceFlowers_test.go
+++ algorithm/leetcode.com/canPlaceFlowers_test.go
@@ -8,9 +8,14 @@ import (
 
 func Test_canPlaceFlowers(t *testing.T) {
 	Convey("测试花坛放置", t, func() {
+		So(canPlaceFlowers([]int{1}, 0), ShouldEqual, true)
 		So(canPlaceFlowers([]int{0}, 1), ShouldEqual, true)
 		So(canPlaceFlowers([]int{0, 0}, 1), ShouldEqual, true)
 		So(canPlaceFlowers([]int{0, 0}, 2), ShouldNotEqual, true)
+		So(canPlaceFlowers([]int{0, 0, 0}, 2), ShouldEqual, true)
+		So(canPlaceFlowers([]int{0, 0, 1}, 1), ShouldEqual, true)
+		So(canPlaceFlowers([]int{0, 1, 0}, 1), ShouldNotEqual, true)
+		So(canPlaceFlowers([]int{1, 0, 0}, 1), ShouldEqual, true)
 		So(canPlaceFlowers([]int{0, 1, 0, 1}, 1), ShouldEqual, false)
 		So(canPlaceFlowers([]int{0, 0, 1, 0, 0}, 2), ShouldEqual, true)
 	})

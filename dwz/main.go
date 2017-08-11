49ed60676903b680f17e622af73d61a5394de875
yidane <yidane@163.com>
2017-08-11 15:10:16 +0800

    update dwz

--- dwz/main.go
+++ dwz/main.go
@@ -7,13 +7,25 @@ import (
 )
 
 func main() {
-	url := "http://dwz.cn/5CFTHF"
-	location, err := GetRedirectUrl(url)
-	if err != nil {
-		fmt.Println(err)
+
+	for i := 100000000; i < 100000001; i++ {
+		url := "http://dwz.cn/" + Generate(i)
+		location, err := GetRedirectUrl(url)
+		if err != nil {
+			fmt.Println(url)
+			fmt.Println(err)
+		}
+
+		fmt.Println(location)
 	}
 
-	fmt.Println(location)
+	//url := "http://dwz.cn/5CFTHF"
+	//location, err := GetRedirectUrl(url)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//fmt.Println(location)
 }
 
 func GetRedirectUrl(url string) (string, error) {
@@ -38,4 +50,47 @@ func GetRedirectUrl(url string) (string, error) {
 	}
 
 	return "", errors.New("The Response StatusCode is not 302")
+}
+
+func Generate(num int) (tiny string) {
+	fmt.Println(num)
+	alpha := merge(getRange(48, 57), getRange(65, 90))
+	alpha = merge(alpha, getRange(97, 122))
+	if num < 62 {
+		tiny = string(alpha[num])
+		return tiny
+	} else {
+		var runes []rune
+		runes = append(runes, alpha[num%62])
+		num = num / 62
+		for num >= 1 {
+			if num < 62 {
+				runes = append(runes, alpha[num-1])
+			} else {
+				runes = append(runes, alpha[num%62])
+			}
+			num = num / 62
+
+		}
+		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+			runes[i], runes[j] = runes[j], runes[i]
+		}
+		tiny = string(runes)
+		return tiny
+	}
+	return tiny
+}
+
+func getRange(start, end rune) (ran []rune) {
+	for i := start; i <= end; i++ {
+		ran = append(ran, i)
+	}
+	return ran
+}
+
+func merge(a, b []rune) []rune {
+	c := make([]rune, len(a)+len(b))
+	copy(c, a)
+	copy(c[len(a):], b)
+	return c
 }

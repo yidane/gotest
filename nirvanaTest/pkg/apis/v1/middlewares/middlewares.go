1825e4ca1667d5e807fce17b6c54ef592b62cb41
yidane <yidane@163.com>
2019-04-15 14:22:36 +0800

    add test for nirvana

---
+++ nirvanaTest/pkg/apis/v1/middlewares/middlewares.go
@@ -0,0 +1,10 @@
+package middlewares
+
+import (
+	def "github.com/caicloud/nirvana/definition"
+)
+
+// Middlewares returns a list of middlewares.
+func Middlewares() []def.Middleware {
+	return []def.Middleware{}
+}
